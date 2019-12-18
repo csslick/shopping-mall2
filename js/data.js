3527b09ae1946bc040de92968300d57968799d6b
csslick <digitone@naver.com>
2019-12-18 22:35:16 +0900

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -10,21 +10,21 @@ var product_data = [
     img_url: "http://placehold.it/225x323&text=2",
     title: "PLASTIC ISLAND",
     name: "[19SS신상] [BEIGE]데님 와이드 팬츠 PK7DP201",
-    origin_price: " ",
+    origin_price: "",
     sale_price: "59,000"
   },
   {
     img_url: "http://placehold.it/225x323&text=3",
     title: "LAP",
     name: "패미닌 레이스 티셔츠 EK4CL962",
-    origin_price: " ",
+    origin_price: "",
     sale_price: "148,000"
   },
   {
     img_url: "http://placehold.it/225x323&text=4",
     title: "EGOIST",
     name: "레이스 테이프 롱 원피스 EK4WO262",
-    origin_price: " ",
+    origin_price: "",
     sale_price: "398,000"
   },
   {
